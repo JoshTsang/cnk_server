201b60150e85bf46958f4fdc179845916c4c367b
Josh <mee990@gmail.com>
2012-06-22 21:12:47 +0800

    1.Implemented getDishStatus

---
+++ macros.php
@@ -0,0 +1,19 @@
+<?php
+	/* Error Num */
+	define('ERR_COULD_NOT_CONECT_DB', "501");
+	define("ERR_DB_QUERY", "502");
+	
+	
+	/* Database */
+    define('DATABASE_MENU', "../db/menu.db");
+	
+	define('CATEGROY_TABLE', "table_show");
+	define('CATEGROY_TABLE_COLUM_TABLE_NAME', "tablename");
+	define('CATEGROY_TABLE_COLUM_ID', "id");
+	define('DISHES_TABLE_COLUM_ID', "id");
+	define('DISHES_TABLE_COLUM_STATUS', "status");
+	
+	/* Dish status */
+	define('DISH_STATUS_SOLD_OUT', "0");
+	
+?>
