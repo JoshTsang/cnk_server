feb8a4d33d5f00d4bfd1605c5a8f2151fdb8d3ec
Josh <mee990@gmail.com>
2012-06-24 11:29:58 +0800

    1.implemented submit order.

--- macros.php
+++ macros.php
@@ -2,6 +2,7 @@
 	/* Error Num */
 	define('ERR_COULD_NOT_CONECT_DB', "501");
 	define("ERR_DB_QUERY", "502");
+	define("NO_ORDERED_DISH", "503");
 	
 	
 	/* Database */
@@ -12,6 +13,18 @@
 	define('CATEGROY_TABLE_COLUM_ID', "id");
 	define('DISHES_TABLE_COLUM_ID', "id");
 	define('DISHES_TABLE_COLUM_STATUS', "status");
+	
+	define('DATABASE_ORDER', "../db/order.db");
+	define('TABLE_ORDER_TABLE', "table_order");
+	define('TABLE_ORDER_TABLE_COLUM_ID', "id");
+	define('TABLE_ORDER_TABLE_COLUM_TABLE_ID', "table_id");
+	define('TABLE_ORDER_TABLE_COLUM_TIMESTAMP', "timestamp");
+	
+	define('ORDER_DETAIL_TABLE', "order_detail");
+	define('ORDER_DETAIL_TABLE_COLUM_DISH_ID', "dish_id");
+	define('ORDER_DETAIL_TABLE_COLUM_PRICE', "price");
+	define('ORDER_DETAIL_TABLE_ORDER_ID', "order_id");
+	define('ORDER_DETAIL_TABLE_QUANTITY', "quantity");
 	
 	/* Dish status */
 	define('DISH_STATUS_SOLD_OUT', "0");
--- submitOrder.php
+++ submitOrder.php
@@ -1,3 +1,51 @@
 <?php
-    phpinfo();
+	require('macros.php');
+	$json_string = $_POST['json'];
+	//$json_string =  " {\"order\":[{\"quan\":1,\"id\":2,\"price\":12},{\"quan\":1,\"id\":3,\"price\":12},{\"quan\":1,\"id\":5,\"price\":34}],\"timestamp\":\"2012-06-23 20:22:38\",\"tableId\":0}";
+	$obj = json_decode($json_string); 
+	$dishCount = count($obj->order);
+	$tableId = $obj->tableId;
+	$timestamp = $obj->timestamp;
+	if ($dishCount <= 0) {
+		header("HTTP/1.1 NO_ORDERED_DISH 'NO_ORDERED_DISH'");
+		exit();
+	}
+	
+	$dbOrder = new SQLite3(DATABASE_ORDER);
+	if (!$dbOrder) {
+		header("HTTP/1.1 ERR_COULD_NOT_CONECT_DB 'ERR_COULD_NOT_CONECT_DB'");
+	  	die(ERR_COULD_NOT_CONECT_DB);
+	}
+	
+	if (!$dbOrder->exec("INSERT INTO ".TABLE_ORDER_TABLE."(".TABLE_ORDER_TABLE_COLUM_TABLE_ID.",". 
+									 TABLE_ORDER_TABLE_COLUM_TIMESTAMP.")".
+						"values('$tableId', '$timestamp')")){
+		header("HTTP/1.1 ERR_DB_QUERY 'ERR_DB_QUERY'");
+		die(ERR_COULD_NOT_CONECT_DB);
+	}
+	
+	$resultSet = $dbOrder->query("SELECT MAX(".TABLE_ORDER_TABLE_COLUM_ID.") from ".
+								  TABLE_ORDER_TABLE." WHERE ".TABLE_ORDER_TABLE_COLUM_TABLE_ID."=$tableId");
+	if ($resultSet) {
+		if ($row = $resultSet->fetchArray()) {
+			$orderId = $row[0];
+		} else {
+			die(0);
+		}
+	} else {
+		die(ERR_DB_QUERY);
+	}
+	
+	for ($i=0; $i<$dishCount; $i++) {
+		$dishId = $obj->order[$i]->id;
+		$price = $obj->order[$i]->price;
+		$dishQuantity = $obj->order[$i]->quan;
+		if (!$dbOrder->exec("INSERT INTO ".ORDER_DETAIL_TABLE."(".ORDER_DETAIL_TABLE_COLUM_DISH_ID.",".
+															ORDER_DETAIL_TABLE_COLUM_PRICE.",".
+															ORDER_DETAIL_TABLE_QUANTITY.",".
+															ORDER_DETAIL_TABLE_ORDER_ID.")".
+							 "values($dishId, $price, $dishQuantity, $orderId)")) {
+			die(0);
+		}
+	}
 ?>
