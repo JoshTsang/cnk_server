9e2e2197dded03ef5b0cf152ad03ee53a2b62440
Josh <mee990@gmail.com>
2012-07-06 15:05:27 +0800

    1.updated layout of receipt.

--- print.php
+++ print.php
@@ -52,12 +52,31 @@ function printerStatus() {
 }
 
 function printc($socket, $str) {
-	if (strlen($str)>15) {
-		$str = substr($str, 0, 15);
-	}
+	// if (strlen($str)>15) {
+		// $str = substr($str, 0, 15);
+	// }
 	$print = iconv("UTF-8","GB2312", $str);
 	socket_write($socket, $print);
-	socket_write($socket, "\x09");
+	socket_write($socket, "\r\n");
+}
+
+function printOrder($socket, $tableId, $timestamp, $obj, $total) {
+	printHeader($socket, $tableId, $timestamp);
+	
+	$dishCount = count($obj->order);
+	$total = 0;
+	for ($i=0; $i<$dishCount; $i++) {
+		$dishId = $obj->order[$i]->id;
+		$price = $obj->order[$i]->price;
+		$dishQuantity = $obj->order[$i]->quan;
+		$dishName = $obj->order[$i]->name;
+		$total += $price * $dishQuantity;
+		//socket_write($socket, "\x1b\x44\x0b\x12\x00");
+		printc($socket, $dishName);
+		$printString = sprintf("            %2.2f%4d% 10.2f", $price, $dishQuantity, $price*$dishQuantity);
+		printl($socket, $printString);
+	}
+	printFooter($socket, $total);
 }
 
 function printJson($print) {
@@ -71,70 +90,18 @@ function printJson($print) {
 	$timestamp = $obj->timestamp;
 	$total = 0;
 	
-	printTitle($socket, "存根联\r\n");
-	printHeader($socket, $tableId, $timestamp);
-	
 	if ($dishCount <= 0) {
 		header("HTTP/1.1 NO_ORDERED_DISH 'NO_ORDERED_DISH'");
 		exit();
 	}
-	/*
-	for ($i=0; $i<$dishCount; $i++) {
-		$dishId = $obj->order[$i]->id;
-		$price = $obj->order[$i]->price;
-		$dishQuantity = $obj->order[$i]->quan;
-		$dishName = $obj->order[$i]->name;
-		$total += $price * $dishQuantity;
-		$printString = sprintf("%s|%6.2f|%3d|%6.2f", $dishName, $price, $dishQuantity, $price*$dishQuantity);
-		printl($socket, $printString);
-	}
-	*/
-	for ($i=0; $i<$dishCount; $i++) {
-		$dishId = $obj->order[$i]->id;
-		$price = $obj->order[$i]->price;
-		$dishQuantity = $obj->order[$i]->quan;
-		$dishName = $obj->order[$i]->name;
-		$total += $price * $dishQuantity;
-		socket_write($socket, "\x1b\x44\x0b\x12\x00");
-		printc($socket, $dishName);
-		$printString = sprintf("%6.2f%4d% 10.2f", $price, $dishQuantity, $price*$dishQuantity);
-		printl($socket, $printString);
-	}
-	printFooter($socket, $total);
+	printTitle($socket, "存根联\r\n");
+	printOrder($socket, $tableId, $timestamp, $obj, $total);
 	
 	//TODO print 1 copy for debug
 	exit(0);
 	printTitle($socket, "客户联\r\n");
-	printHeader($socket, $tableId, $timestamp);
+	printOrder($socket, $tableId, $timestamp, $obj, $total);
 	
-	if ($dishCount <= 0) {
-		header("HTTP/1.1 NO_ORDERED_DISH 'NO_ORDERED_DISH'");
-		exit();
-	}
-	/*
-	for ($i=0; $i<$dishCount; $i++) {
-		$dishId = $obj->order[$i]->id;
-		$price = $obj->order[$i]->price;
-		$dishQuantity = $obj->order[$i]->quan;
-		$dishName = $obj->order[$i]->name;
-		$total += $price * $dishQuantity;
-		$printString = sprintf("%s|%6.2f|%3d|%6.2f", $dishName, $price, $dishQuantity, $price*$dishQuantity);
-		printl($socket, $printString);
-	}
-	*/
-	$total = 0;
-	for ($i=0; $i<$dishCount; $i++) {
-		$dishId = $obj->order[$i]->id;
-		$price = $obj->order[$i]->price;
-		$dishQuantity = $obj->order[$i]->quan;
-		$dishName = $obj->order[$i]->name;
-		$total += $price * $dishQuantity;
-		socket_write($socket, "\x1b\x44\x0b\x12\x00");
-		printc($socket, $dishName);
-		$printString = sprintf("%6.2f%4d% 10.2f", $price, $dishQuantity, $price*$dishQuantity);
-		printl($socket, $printString);
-	}
-	printFooter($socket, $total);
 	socket_close($socket);
 }
 ?>
