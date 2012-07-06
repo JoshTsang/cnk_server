69e9fc17fcbfe43375262a10143d8102ef9cbe0f
Jone <likangjing.bobo@gmail.com>
2012-07-06 19:17:09 +0800

    1.add function printJsonDel in print.php 2.modify DelOrder can print.

--- DelOrder.php
+++ DelOrder.php
@@ -1,19 +1,24 @@
 <?php
 	require('macros.php');
+	require('print.php');
 	
 	$dbTable = new SQLite3(DATABASE_ORDER);
+	
 	if (!$dbTable) {
 		header("HTTP/1.1 ERR_COULD_NOT_CONECT_DB 'ERR_COULD_NOT_CONECT_DB'");
 	  	die(ERR_COULD_NOT_CONECT_DB);
 	}
 
-
-	$DishId = $_GET['DID'];
+	
+	
+	$json_string = $_GET['JSON'];
+	$obj = json_decode($json_string); 
+	$DishId = $obj->id;
 	$sql=sprintf("DELETE from %s where %s = %d",
 				 ORDER_DETAIL_TABLE,
-				 ORDER_DETAIL_TABLE_COLUM_DISH_ID,$DishId);
+				 ORDER_DETAIL_TABLE_COLUM_ID,$DishId);
 				 
 	$dbTable->query($sql);
 	$dbTable->close();
-	
+	printJsonDel($json_string);
 ?>
