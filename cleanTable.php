b663379ae7dfb3e7fd8f36dc6658671854d31a81
Jone <likangjing.bobo@gmail.com>
2012-09-21 14:24:44 +0800

    1.fix cleanTable.php checkout clean more table bug

--- cleanTable.php
+++ cleanTable.php
@@ -7,10 +7,19 @@
 	}
 	$json_string = $_POST['json'];
 	$obj = json_decode($json_string);
-	$tableId = $obj->TID;
-	$timestamp = $obj->timestamp;
-	
 	$db = new CNK_DB();
-	$db->cleanTable($tableId, $timestamp);
+	
+	
+	$order = $obj->order;
+	$timestamp = $obj->timestamp;
+	$dishCount = count($obj->order);
+	if ($dishCount <= 0) {
+	  	die("[MORE_PARAM_NEEDED:".MORE_PARAM_NEEDED."]");
+	}
+	for($i = 0;$i < $dishCount;$i++){
+		$tableId = $order[$i] -> TID;
+		$db->cleanTable($tableId, $timestamp);
+	}
 	echo $db->error();
+	
 ?>
